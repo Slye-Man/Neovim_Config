b4e742d62c4bbc513ad125e0f182e58fbc291ed6
Slye-Man <selaelosebola67@gmail.com>
2024-10-28 11:09:38 +0200

    added neotree and nvim-treesitter plugins alongside with their dependencies

--- init.lua
+++ init.lua
@@ -26,7 +26,16 @@ local plugins = {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
       dependencies = { 'nvim-lua/plenary.nvim' }
     },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", 
+      "MunifTanjim/nui.nvim",
+    }
+  }
 }
 
 --lazy.vim call on startup
@@ -36,6 +45,10 @@ require("lazy").setup(plugins)
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+--keymap for Neotree
+vim.keymap.set('n', '<C-n>', ':Neotree<CR>')
+vim.keymap.set('n', '<C-c>', ':Neotree close<CR>')
 
 --enables language parsers
 local config = require("nvim-treesitter.configs")
